d94cf4fc905c737834d8e88bb0b58fb09a3023cf
Maksim <maks4tmk5423@yandex.ru>
2024-04-11 23:45:19 +0300

    initial commit

---
+++ Задание 2 Параллельные вычисления/Задание 2 Параллельные вычисления.cpp
@@ -0,0 +1,12 @@
+﻿// Задание 2 Параллельные вычисления.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <thread>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	std::cout << "Количество аппаратных ядер - " << std::thread::hardware_concurrency() << std::endl;
+}
+
